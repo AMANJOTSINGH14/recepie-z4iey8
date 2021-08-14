2fd04973ab7115481503e7451d31afa5c48c95e0
AMANJOTSINGH14 <singhamanjot95@yahoo.com>
2021-08-15 03:01:40 +0530

    2 files modified

--- src/app/recepie/recepie-list/recepie-item/recepie-item.component.ts
+++ src/app/recepie/recepie-list/recepie-item/recepie-item.component.ts
@@ -1,4 +1,5 @@
 import { Component,Input} from '@angular/core';
+import { Router } from '@angular/router';
 import{Recepie} from '../../recepie.model';
 import { recepieservice } from '../../recepie.service';
 @Component({
@@ -7,9 +8,17 @@ import { recepieservice } from '../../recepie.service';
   
 })
 export class RecepieItemComponent {
-@Input() recepie: Recepie;
+ids:string
+  @Input() recepie: Recepie;
 @Input() index: number;
-  constructor(private recpieService:recepieservice) { }
+  constructor(private recpieService:recepieservice, private router:Router) { }
 
- 
+ doubleTap(id){
+  if(this.ids===id){
+    this.router.navigate(['/recipes']);
+    this.ids=null;
+  }else{
+    this.router.navigate(['/recipes',id]);
+    this.ids=id;}  
+ }
 }
